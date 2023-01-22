397f1fc97c59035f5021dc2733653efc2be309ef
unknown <cristiangvramos1313@gmail.com>
2023-01-22 11:12:49 -0600

    [FIX] Proyecto completo

--- vite-project/src/Containers/App.jsx
+++ vite-project/src/Containers/App.jsx
@@ -1,18 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
 import Home from "../pages/Home";
-/* import Login from "../pages/Login";
-import Register from '../pages/Register'; */
-/* import FlexBox from '../components/molecules/FlexBox'; */
 
 function App() {
     return ( 
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>}/>
-            {/* <Route path="/Login" element={<Login/>}/>
-            <Route path='/Register' element={<Register/>}/> */}
-            {/* <Route path='/flexbox' element={<FlexBox/>}/> */}
         </Routes>
         </BrowserRouter>
      );
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,31 +1,11 @@
-//import Title from "../components/atomos/title"
 import { Link } from "react-router-dom";
 import Header from "../Components/atoms/Header";
 import Experiencia from "../Components/atoms/Experiencia";
 import Estudios from "../Components/atoms/Estudios";
 import Habilidades from "../Components/atoms/Habilidades";
 import Footer from "../Components/atoms/Footer";
-/* import ListStudent from "../components/molecules/ListStudent"; */
 
 function Home(){
-    /* const listStudent = [
-        {
-            id:1,
-            name:"cristian",
-        },{
-            id:2,
-            name:"gerardo",
-        },{
-            id:3,
-            name:"yahir",
-        },{
-            id:4,
-            name:"cesar",
-        },{
-            id:5,
-            name:"carlos"
-            }
-    ] */
     return(
         <>
         <Header/>
@@ -33,9 +13,6 @@ function Home(){
         <Estudios/>
         <Habilidades/>
         <Footer/>
-        {/* <Flex/>
-        <ListStudent listStudent={listStudent}></ListStudent> */}
-        {/* <Link to="/Login">Iniciar Sesion</Link> */}
         </>
     )
 }
